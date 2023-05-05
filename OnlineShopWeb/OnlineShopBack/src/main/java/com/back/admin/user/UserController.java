6388f328d6957d0e5d5150f6d9afc61efac3f90a
Gilles Patrick Ngondo <ngondogillespatrick@gmail.com>
2023-05-05 07:53:31 -0400

    Fix all the issues about user form like cancel button, enable field and role checkbox

--- OnlineShopWeb/OnlineShopBack/src/main/java/com/back/admin/user/UserController.java
+++ OnlineShopWeb/OnlineShopBack/src/main/java/com/back/admin/user/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -32,6 +33,7 @@ public class UserController {
     public String newUser(Model model) {
         List<Role> listRoles = userService.listRoles();
         User user = new User();
+        user.setEnabled(true);
         model.addAttribute("user", user);
         model.addAttribute("listRoles", listRoles);
         return "newUser";
@@ -39,8 +41,10 @@ public class UserController {
 
     // 
     @PostMapping("/users/save")
-    public String saveUser(User user) {
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
         System.out.println(user);
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("message", "User has been saved successfully ...");
         return "redirect:/users";
     }
 
